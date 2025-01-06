576a8c444c762bb8bf5412e76277bd1229c7b283
beyarkay <boydrkane@gmail.com>
2025-01-06 16:26:16 +0000

    Add GH-based comments

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        // from data-repo
+        repo: "beyarkay/website",
+        // from data-repo-id
+        repoId: "R_kgDOMglifg",
+        // from data-category
+        category: "Announcements",
+        // from data-category-id
+        categoryId: "DIC_kwDOMglifs4Clyjj",
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
