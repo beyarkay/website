f584620ab0218bf584bd6fa991520bf705600c17
beyarkay <boydrkane@gmail.com>
2025-04-13 14:05:48 +0200

    Modify 404 page

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -10,6 +10,11 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <p>
+        Please note that I semi-frequently link to essays that are still being written, and track
+        when these links get clicked. By clicking the link, you've let me know what to prioritise in
+        my writing. Thanks!
+      </p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
